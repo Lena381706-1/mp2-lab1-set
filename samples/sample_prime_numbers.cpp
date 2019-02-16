173a2ac983c399797420d14a117b6713f11a5545
Елена Голованова <43187051+Lena381706-1@users.noreply.github.com>
2019-02-16 16:15:15 +0300

    П О Ж А Л У Й С Т А, Л И Ш Н И Е Ф А Й Л Ы, У Й Д И Т Е

--- samples/sample_prime_numbers.cpp
+++ samples/sample_prime_numbers.cpp
@@ -8,7 +8,7 @@
 #include <iomanip>
 
 // #define USE_SET // Использовать класс TSet,
-                // закоментировать, чтобы использовать битовое поле
+// закоментировать, чтобы использовать битовое поле
 
 #ifndef USE_SET // Использовать класс TBitField
 
@@ -16,39 +16,55 @@
 
 int main()
 {
-  int n, m, k, count;
+	int n, m, k, count;
 
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки битового поля" << endl;
-  cout << "             Решето Эратосфена" << endl;
-  cout << "Введите верхнюю границу целых значений - ";
-  cin  >> n;
-  TBitField s(n + 1);
-  // заполнение множества
-  for (m = 2; m <= n; m++)
-    s.SetBit(m);
-  // проверка до sqrt(n) и удаление кратных
-  for (m = 2; m * m <= n; m++)
-    // если m в s, удаление кратных
-    if (s.GetBit(m))
-      for (k = 2 * m; k <= n; k += m)
-        if (s.GetBit(k))
-          s.ClrBit(k);
-  // оставшиеся в s элементы - простые числа
-  cout << endl << "Печать множества некратных чисел" << endl << s << endl;
-  cout << endl << "Печать простых чисел" << endl;
-  count = 0;
-  k = 1;
-  for (m = 2; m <= n; m++)
-    if (s.GetBit(m))
-    {
-      count++;
-      cout << setw(3) << m << " ";
-      if (k++ % 10 == 0)
-        cout << endl;
-    }
-  cout << endl;
-  cout << "В первых " << n << " числах " << count << " простых" << endl;
+	setlocale(LC_ALL, "Russian");
+	cout << "Тестирование программ поддержки битового поля" << endl;
+	cout << "             Решето Эратосфена" << endl;
+	cout << "Введите верхнюю границу целых значений - ";
+	cin >> n;
+	TBitField s(n + 1);
+	// заполнение множества
+	for (m = 2; m <= n; m++)
+		s.SetBit(m);
+	// проверка до sqrt(n) и удаление кратных
+	for (m = 2; m * m <= n; m++)
+		// если m в s, удаление кратных
+		if (s.GetBit(m))
+			for (k = 2 * m; k <= n; k += m)
+				if (s.GetBit(k))
+					s.ClrBit(k);
+	// оставшиеся в s элементы - простые числа
+	cout << endl << "Печать множества некратных чисел" << endl << s << endl;
+	cout << endl << "Печать простых чисел" << endl;
+	count = 0;
+	k = 1;
+	for (m = 2; m <= n; m++)
+		if (s.GetBit(m))
+		{
+			count++;
+			cout << setw(3) << m << " ";
+			if (k++ % 10 == 0)
+				cout << endl;
+		}
+	cout << endl;
+	cout << "В первых " << n << " числах " << count << " простых" << endl;
+
+	//Проверка работы операторов ввода/вывода
+	cout << "\n\t<<< Ввод битового поля целых чисел >>>\n";
+	cout << "Введите наибольший элемент битового поля\n";
+	cin >> n;
+	TBitField _bit(n + 1);
+	cout << "Введите битовое поле\n";
+	cin >> _bit;
+	cout << "\nВывод битового поля\n";
+	cout << _bit;
+	cout << "\n\nВывод битового поля в целых числах\n";
+	for (m = 0; m <= n; m++)
+	{
+		if (_bit.GetBit(m))
+			cout << setw(3) << m << " ";
+	}
 }
 #else
 
@@ -56,39 +72,55 @@ int main()
 
 int main()
 {
-  int n, m, k, count;
+	int n, m, k, count;
 
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование программ поддержки множества" << endl;
-  cout << "              Решето Эратосфена" << endl;
-  cout << "Введите верхнюю границу целых значений - ";
-  cin  >> n;
-  TSet s(n + 1);
-  // заполнение множества
-  for (m = 2; m <= n; m++)
-    s.InsElem(m);
-  // проверка до sqrt(n) и удаление кратных
-  for (m = 2; m * m <= n; m++)
-    // если м в s, удаление кратных
-    if (s.IsMember(m))
-      for (k = 2 * m; k <= n; k += m)
-       if (s.IsMember(k))
-         s.DelElem(k);
-  // оставшиеся в s элементы - простые числа
-  cout << endl << "Печать множества некратных чисел" << endl << s << endl;
-  cout << endl << "Печать простых чисел" << endl;
-  count = 0;
-  k = 1;
-  for (m = 2; m <= n; m++)
-    if (s.IsMember(m))
-    {
-      count++;
-      cout << setw(3) << m << " ";
-      if (k++ % 10 == 0)
-        cout << endl;
-    }
-  cout << endl;
-  cout << "В первых " << n << " числах " << count << " простых" << endl;
+	setlocale(LC_ALL, "Russian");
+	cout << "Тестирование программ поддержки множества" << endl;
+	cout << "              Решето Эратосфена" << endl;
+	cout << "Введите верхнюю границу целых значений - ";
+	cin >> n;
+	TSet s(n + 1);
+	// заполнение множества
+	for (m = 2; m <= n; m++)
+		s.InsElem(m);
+	// проверка до sqrt(n) и удаление кратных
+	for (m = 2; m * m <= n; m++)
+		// если м в s, удаление кратных
+		if (s.IsMember(m))
+			for (k = 2 * m; k <= n; k += m)
+				if (s.IsMember(k))
+					s.DelElem(k);
+	// оставшиеся в s элементы - простые числа
+	cout << endl << "Печать множества некратных чисел" << endl << s << endl;
+	cout << endl << "Печать простых чисел" << endl;
+	count = 0;
+	k = 1;
+	for (m = 2; m <= n; m++)
+		if (s.IsMember(m))
+		{
+			count++;
+			cout << setw(3) << m << " ";
+			if (k++ % 10 == 0)
+				cout << endl;
+		}
+	cout << endl;
+	cout << "В первых " << n << " числах " << count << " простых" << endl;
+
+	//Проверка работы операторов ввода/вывода
+	cout << "\n\t<<< Ввод множества целых чисел >>>\n";
+	cout << "Введите наибольший элемент множества\n";
+	cin >> n;
+	TSet _bit(n + 1);
+	cout << "Введите элементы множества\n";
+	cin >> _bit;
+	cout << "\nВывод множества в виде битового поля\n";
+	cout << _bit;
+	cout << "\n\nВывод множества в целых числах\n";
+	for (m = 0; m <= n; m++)
+	{
+		if (_bit.IsMember(m))
+			cout << setw(3) << m << " ";
+	}
 }
 
 #endif
